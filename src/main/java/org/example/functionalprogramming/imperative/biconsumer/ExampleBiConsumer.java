0a13abaca16fedcaf66f3018cf1cfc0160369af2
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-12-20 02:02:39 +0400

    Biconsumer functional

---
+++ src/main/java/org/example/functionalprogramming/imperative/biconsumer/ExampleBiConsumer.java
@@ -0,0 +1,25 @@
+package org.example.functionalprogramming.imperative.biconsumer;
+
+import org.example.functionalprogramming.imperative.Gender;
+import org.example.functionalprogramming.imperative.Person;
+
+import java.util.function.BiConsumer;
+
+public class ExampleBiConsumer {
+
+    public static void main(String[] args) {
+        getBiConsumer.accept(new Person("Samir", "Rehimov", Gender.MALE), false);
+        getWithPhoneConsumer.accept(new Person("Eli", "Samirov", Gender.MALE), true);
+    }
+
+
+    static BiConsumer<Person, Boolean> getBiConsumer =
+            (person, isResult) -> {
+                System.out.println(isResult ? person.getName() + " " + person.getSurname() : person.getGender() + "***************");
+            };
+
+    static BiConsumer<Person, Boolean> getWithPhoneConsumer =
+            (person, isPhone) -> {
+                System.out.println(isPhone ? person.getName() + " " + person.getSurname() : person.getGender() + "***************");
+            };
+}
