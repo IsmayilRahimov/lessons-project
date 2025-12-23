804a3a9919133a48adf33426612e57624603d599
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-12-23 15:42:33 +0400

    consumer function

---
+++ src/main/java/org/example/imperative/functional/BiConsumerTime.java
@@ -0,0 +1,16 @@
+package org.example.imperative.functional;
+
+import org.example.imperative.Person;
+
+import java.util.function.BiConsumer;
+
+public class BiConsumerTime {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    static BiConsumer<Person,String>
+}
