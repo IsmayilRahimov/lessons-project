d5eb44e952cc99abaeea96ee0d736180ca9258e3
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-12-24 16:12:35 +0400

    functions

---
+++ src/main/java/org/example/properties/ApplicationConstant.java
@@ -0,0 +1,17 @@
+package org.example.properties;
+
+import org.example.functionalprogramming.imperative.consumer.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ApplicationConstant {
+
+
+    public static final List<Person> persons = new ArrayList<>();
+
+
+    private ApplicationConstant() {
+
+    }
+}
