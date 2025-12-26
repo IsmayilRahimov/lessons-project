e08e3523d4a24c6a3dfd8fb94592f3f6c6b9db6f
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-12-26 17:34:23 +0400

    biconsumer map key value

--- src/main/java/org/example/functional/biconsumer/Biconssumer.java
+++ src/main/java/org/example/functional/biconsumer/Biconssumer.java
@@ -1,11 +1,22 @@
 package org.example.functional.biconsumer;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 public class Biconssumer {
 
 
     public static void main(String[] args) {
+
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+
+        map.forEach((k, v) -> {
+            System.out.println(k + "=" + v);
+        });
 
         apply.accept(1, 2);
     }
