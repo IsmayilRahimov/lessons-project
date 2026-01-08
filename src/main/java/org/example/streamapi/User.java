9c1a7b5cc49089c2e517587eec2bf05a9ba77534
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-01-08 18:12:09 +0400

    stream api with method reference collect to list

--- src/main/java/org/example/streamapi/User.java
+++ src/main/java/org/example/streamapi/User.java
@@ -2,6 +2,7 @@ package org.example.streamapi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
 
@@ -15,10 +16,6 @@ public class User {
 
     @Override
     public String toString() {
-        System.out.println("User{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                '}');
         return "User{" +
                 "name='" + name + '\'' +
                 ", id=" + id +
@@ -28,13 +25,19 @@ public class User {
     public static void main(String[] args) {
 
         List<User> list = new ArrayList<>();
-        list.add(new User("Samir",1));
-        list.add(new User("Ismayil",2));
-        list.add(new User("Sahib",3));
-        list.add(new User("Musa",4));
+        list.add(new User("Samir", 1));
+        list.add(new User("Ismayil", 2));
+        list.add(new User("Sahib", 3));
+        list.add(new User("Musa", 4));
 
-//        list.stream().forEach(System.out::println);
-        list.stream().forEach(User::toString); //method reference
+        list.stream().forEach(User::toString);//method reference
+        list.stream().forEach(System.out::println);
+
+        List<User> filtered = list.stream()
+                .filter(user -> user.id > 2)
+                .collect(Collectors.toList());
+        System.out.println(filtered);
+
 
     }
 }
