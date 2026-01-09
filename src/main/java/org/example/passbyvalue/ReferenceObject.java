96016ad5bfe9ebae77ff6d9277ee8a1a57cbdca5
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-01-09 16:08:05 +0400

    null object reference

---
+++ src/main/java/org/example/passbyvalue/ReferenceObject.java
@@ -0,0 +1,18 @@
+package org.example.passbyvalue;
+
+public class ReferenceObject {
+
+    public static void main(String[] args) {
+        ReferenceObject obj = null;
+
+        try {
+            obj.toString();
+        } catch (NullPointerException e) {
+
+            System.out.println(e.toString());
+        } finally {
+            System.out.println();
+        }
+    }
+
+}
