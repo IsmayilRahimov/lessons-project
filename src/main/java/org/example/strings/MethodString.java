0e9054a0f8a275530306ba5229f2294438a9feaa
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-01-11 20:24:43 +0400

    integer methods

---
+++ src/main/java/org/example/strings/MethodString.java
@@ -0,0 +1,12 @@
+package org.example.strings;
+
+public class MethodString {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        String m = String.valueOf(a);
+        System.out.println(m);
+    }
+}
