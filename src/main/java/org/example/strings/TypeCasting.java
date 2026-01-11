7be81bb3bc673fc17890962212b685df43c53753
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-01-11 20:58:56 +0400

    type casting

---
+++ src/main/java/org/example/strings/TypeCasting.java
@@ -0,0 +1,23 @@
+package org.example.strings;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        double b = a;
+        System.out.println(b);
+
+
+        double m = 5.7;
+        int c = (int) m;
+        System.out.println(c);
+
+        float f = 3.15F;
+
+        int d = (int) f;
+        System.out.println(d);
+
+
+    }
+}
