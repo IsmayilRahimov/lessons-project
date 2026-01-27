378e0556e56ad590496fc7abc91ff043c353321a
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-01-27 18:33:23 +0400

    enums

---
+++ src/main/java/org/example/example/TypeEnum.java
@@ -0,0 +1,21 @@
+package org.example.example;
+
+public enum TypeEnum {
+
+
+    STUDENT_READ(),
+    STUDENT_WRITE(),
+    TEACHER_READ(),
+    TEACHER_WRITE();
+
+
+
+    TypeEnum() {
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
