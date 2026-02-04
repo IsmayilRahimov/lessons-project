2261ec9281f6797ccfc4e47a96aa42b4520cbc9a
IsmayilRahimov <rehimovismayil650@gmail.com>
2026-02-04 19:41:43 +0400

    enums

---
+++ src/main/java/org/example/example/WeekTime.java
@@ -0,0 +1,24 @@
+package org.example.example;
+
+public enum WeekTime {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+
+    private Integer day;
+
+    WeekTime(Integer day) {
+        this.day = day;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+
+}
